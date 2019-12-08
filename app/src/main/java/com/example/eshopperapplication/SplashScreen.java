b75d8f9aaa6603719db3e5811a86663a99255ec9
Megha Mahna <meghamahna04@gmail.com>
2019-12-08 14:07:52 -0500

    Java File for Splash Screen

--- app/src/main/java/com/example/eshopperapplication/SplashScreen.java
+++ app/src/main/java/com/example/eshopperapplication/SplashScreen.java
@@ -2,6 +2,7 @@ package com.example.eshopperapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
@@ -10,5 +11,23 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        myThread.start();
     }
 }
