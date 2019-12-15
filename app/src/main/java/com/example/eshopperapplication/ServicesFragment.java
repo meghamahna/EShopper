232ced7b4a30e1db4f96e8ab32602f6d841b8166
Megha Mahna <meghamahna04@gmail.com>
2019-12-14 22:37:07 -0500

    Grid View of Services

--- app/src/main/java/com/example/eshopperapplication/ServicesFragment.java
+++ app/src/main/java/com/example/eshopperapplication/ServicesFragment.java
@@ -3,11 +3,14 @@ package com.example.eshopperapplication;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 
 /**
@@ -15,8 +18,12 @@ import android.view.ViewGroup;
  */
 public class ServicesFragment extends Fragment {
 
+    int[] imageIcons ;
+    String[] imageNames;
+
 
     public ServicesFragment() {
+
         // Required empty public constructor
     }
 
@@ -25,7 +32,17 @@ public class ServicesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        super.onCreate(savedInstanceState);
         return inflater.inflate(R.layout.fragment_services, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        imageIcons = new int[]{R.drawable.real_estate, R.drawable.travel, R.drawable.entertainment, R.drawable.food};
+        imageNames = new String[]{"Real Estate", "Travel", "Entertainment", "Food"};
+        GridView gridView = (GridView)getView().findViewById(R.id.gridView);
+
+        final GridAdapter gridAdapter = new GridAdapter(getActivity(),imageIcons,imageNames);
+        gridView.setAdapter(gridAdapter);
+    }
 }
