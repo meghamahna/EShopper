528363dd1d9ab91bd48cefe91d30a0a5606267fa
Harpreet Kaur <preetjandu0003@gmail.com>
2019-12-15 14:32:47 -0500

    Navigation Drawer Panel

--- app/src/main/java/com/example/eshopperapplication/MainActivity.java
+++ app/src/main/java/com/example/eshopperapplication/MainActivity.java
@@ -1,22 +1,51 @@
 package com.example.eshopperapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     TextView shops, services;
     ViewPager viewPager;
     PagerViewAdapter pagerViewAdapter;
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    NavigationView navigationView;
+    ActionBarDrawerToggle toggle;
 
+    @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        drawerLayout = findViewById(R.id.drawer);
+        toolbar = findViewById(R.id.toolBar);
+        navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawerOpen,R.string.drawerClose);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
 
         shops = (TextView) findViewById(R.id.shop);
         services = findViewById(R.id.services);
@@ -69,6 +98,34 @@ public class MainActivity extends AppCompatActivity {
             shops.setTextSize(25);
             services.setTextSize(30);
         }
+    }
+
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        if (menuItem.getItemId() == R.id.home){
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, new home()).commit();
+        }else if (menuItem.getItemId() == R.id.contact) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, new contact()).commit();
+            Toast.makeText(MainActivity.this, "contact selected", Toast.LENGTH_SHORT).show();
+        } else if (menuItem.getItemId() == R.id.about) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, new about()).commit();
+            Toast.makeText(MainActivity.this, "about selected", Toast.LENGTH_SHORT).show();
+        } else if (menuItem.getItemId() == R.id.feedback){
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, new feedback()).commit();
+            Toast.makeText(MainActivity.this, "feedback selected", Toast.LENGTH_SHORT).show();
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return false;
     }
 
 }
