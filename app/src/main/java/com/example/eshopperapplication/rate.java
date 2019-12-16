0bd6f0cec78ebc7ae00dccdec002c58a6cb90c03
Rajveer Mukkar <krajveer131@gmail.com>
2019-12-15 20:21:17 -0500

    Rate Us Page java

--- app/src/main/java/com/example/eshopperapplication/rate.java
+++ app/src/main/java/com/example/eshopperapplication/rate.java
@@ -1,12 +1,17 @@
 package com.example.eshopperapplication;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 public class rate extends Fragment{
@@ -19,4 +24,54 @@ public class rate extends Fragment{
 
 
         }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        final RatingBar mRatingBar = (RatingBar)getView().findViewById(R.id.ratingBar);
+
+        final TextView mRatingScale = (TextView) getView().findViewById(R.id.tvRatingScale);
+        Button mSendFeedback = (Button)getView(). findViewById(R.id.btnSubmit);
+
+        mRatingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                mRatingScale.setText(String.valueOf(v));
+                switch ((int) ratingBar.getRating()) {
+                    case 1:
+                        mRatingScale.setText("Very bad");
+                        break;
+                    case 2:
+                        mRatingScale.setText("Need some improvement");
+                        break;
+                    case 3:
+                        mRatingScale.setText("Good");
+                        break;
+                    case 4:
+                        mRatingScale.setText("Great");
+                        break;
+                    case 5:
+                        mRatingScale.setText("Awesome. I love it");
+                        break;
+                    default:
+                        mRatingScale.setText("");
+                }
+            }
+        });
+
+
+        mSendFeedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mRatingBar.setRating(0);
+                new AlertDialog.Builder(getActivity()).setTitle(" 😊Thank you for rating us😊").setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+
+                    }
+                }).show();
+            }
+        });
+    }
 }
