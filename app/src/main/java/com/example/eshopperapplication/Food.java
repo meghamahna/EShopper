afa40cc989453df4eb89780c5d607812636110c0
Rajveer Mukkar <krajveer131@gmail.com>
2019-12-15 23:55:36 -0500

    search option for Food

--- app/src/main/java/com/example/eshopperapplication/Food.java
+++ app/src/main/java/com/example/eshopperapplication/Food.java
@@ -8,11 +8,15 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
 
 public class Food extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -25,7 +29,8 @@ public class Food extends AppCompatActivity implements NavigationView.OnNavigati
     int[] imageIcons;
     String[] imageNames;
 
-
+    ArrayList<customclass> newlist = new ArrayList<>();
+    final ListAdapter listAdapter = new ListAdapter(this, newlist);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +38,16 @@ public class Food extends AppCompatActivity implements NavigationView.OnNavigati
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
 
-        imageIcons = new int[] {R.drawable.foodora, R.drawable.skip, R.drawable.ubereats};
-        imageNames = new String[]{"Foodora", "Skip The Dishes", "Uber Eats"};
+        newlist.add(new customclass(R.drawable.foodora,"Foodora"));
+        newlist.add(new customclass(R.drawable.skip,"Skip The Dishes"));
+        newlist.add(new customclass(R.drawable.ubereats,"Uber Eats"));
+
+
 
         listView = findViewById(R.id.listView);
-//
-//        final ListAdapter listAdapter = new ListAdapter(this, imageIcons, imageNames);
-//        listView.setAdapter(listAdapter);
+
+        listView.setAdapter(listAdapter);
+
 
         drawerLayout = findViewById(R.id.drawer);
         toolbar = findViewById(R.id.toolBar);
@@ -71,5 +79,40 @@ public class Food extends AppCompatActivity implements NavigationView.OnNavigati
 
         drawerLayout.closeDrawer(GravityCompat.START);
         return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+
+        getMenuInflater().inflate(R.menu.search_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                ArrayList<customclass> results = new ArrayList<>();
+
+                for (customclass c1 : newlist) {
+                    if (c1.imageNames.contains(newText)) {
+                        results.add(c1);
+                    }
+
+                    ( (ListAdapter)listView.getAdapter()).update(results);
+                }
+
+
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
